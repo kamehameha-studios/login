0f544d46c47486a021e6a006161d0401811f84f3
Bosco Kwong <95505913+boscokwong@users.noreply.github.com>
2022-01-17 13:36:44 -0600

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ function WriteCookie() {
   document.cookie = name = "a"; expires = now; domain = doman; value = cookievalue;
 }
 var checkAccount = function(username, password) {
-  var name = document.getelementbyid("#name");
-  var code = document.getelementbyid("#password");
+  var name = document.GetElementById("#name");
+  var code = document.GetElementById("#password");
   name = username;
   code = password;
 }

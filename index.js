4901ac106adb18be8554181455e28a561769dae5
Bosco Kwong <95505913+boscokwong@users.noreply.github.com>
2022-01-17 09:44:30 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+function WriteCookie() {
+          let now = new date();
+          now.setMonth( now.getMonth() + 11);
+          let doman = "kamehameha-studios.github.io;";
 
+          cookievalue = "";
 
+          document.cookie = name = "a"; expires = now; domain = doman; value = cookievalue;
+        }

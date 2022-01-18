cc91f31e2ba0d0cf4b029e4decb0d03ccee00444
Bosco Kwong <95505913+boscokwong@users.noreply.github.com>
2022-01-18 16:26:40 -0600

    Update index.js

--- index.js
+++ index.js
@@ -10,4 +10,12 @@ function WriteCookie() {
 var checkAccount = function() {
   var name = document.GetElementById("#name");
   var code = document.GetElementById("#password");
+  var form = document.GetElementById("#form");
+  var hidden1 = "";
+  if(name "value" === "Bosco") {
+    form action="https://kamehameha-studios.github.io/Bosco-home/";
+  }
+  if(code "value" === hidden1) {
+    form action="https://kamehameha-studios.github.io/Bosco-home/";
+  }
 }

807f4df200b651e50651f9b0a08565d4166c314c
Bosco Kwong <95505913+boscokwong@users.noreply.github.com>
2022-01-20 17:16:03 -0600

    update index.html, index.css, index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function WriteCookie() {
-          let now = new date();
-          now.setMonth( now.getMonth() + 11);
-          let doman = "kamehameha-studios.github.io;";
+  let now = new date();
+  now.setMonth( now.getMonth() + 11);
+  let doman = "kamehameha-studios.github.io;";
 
-          cookievalue = "";
+  cookievalue = "";
 
-          document.cookie = name = "a"; expires = now; domain = doman; value = cookievalue;
-        }
+  document.cookie = name = "a"; expires = now; domain = doman; value = cookievalue;
+}
 

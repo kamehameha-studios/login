544d7564429519555ae2e9191d4cad66c4405924
bossOfCode <95505913+bossOfCode@users.noreply.github.com>
2022-04-22 08:40:23 -0700

    a

--- index.sql
+++ index.sql
@@ -2,11 +2,22 @@ CREATE TABLE users (
   id INTEGER PRIMARY KEY, 
   username TEXT, 
   password REAL
+  account-type TEXT,
+  account-name TEXT
 );
 
 INSERT INTO users VALUES (
   1, 
   "Bosco", 
-  "opP4^yu86HhjK"
+  "opP4^yu86HhjK",
+  "basic",
+  "admin"
+);
+INSERT INTO users VALUES (
+  2,
+  "Julian",
+  "2920juliansquad",
+  "basic",
+  "member, co-admin"
 );
 SELECT * FROM user;

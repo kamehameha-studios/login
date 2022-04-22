b6fea9614402e760fa9de56236da8867793ccb2f
bossOfCode <95505913+bossOfCode@users.noreply.github.com>
2022-04-22 08:34:23 -0700

    Create index.sql

---
+++ index.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY, 
+  username TEXT, 
+  password REAL
+);
+
+INSERT INTO users VALUES (
+  1, 
+  "Bosco", 
+  "opP4^yu86HhjK"
+);
+SELECT * FROM user;
